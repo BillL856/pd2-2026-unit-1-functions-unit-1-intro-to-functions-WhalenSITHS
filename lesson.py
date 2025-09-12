eaf98825a32735c13f7a529c968f699c11616592
BillL856 <billl14@nycstudents.net>
2025-09-12 09:19:17 -0400

    done

---
+++ lesson.py
@@ -0,0 +1,19 @@
+""" #strings represent characters or text
+x = "dang it Yi"
+#inputs output strings
+name = input("What's your name")
+print(name)
+
+def add(x,y):
+    return x + y
+z = add(5,15)
+print(z)
+
+#integers or numbers
+a = int('5') 
+bill = input("How much was the bill?")
+print(int(bill) * .15) """
+
+""" name = "Mason"
+#use f string
+print(f"His name is {name}") """
